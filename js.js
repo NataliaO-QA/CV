8dbf6ae7367828fc735f1d85644fcb04c966ba06
Natalia <3107800k@gmail.com>
2024-11-20 17:05:33 +0300

    Update js.js

--- js.js
+++ js.js
@@ -3,5 +3,5 @@ const button = document.querySelector('button');
 
 /* Тут на кнопку навешиваем обрабочик, который ждёт клика и тогда запустит логику */
 button.addEventListener('click', function() {
-	alert('img src="./наш кот.jpg" alt="я" class="header__я" height=300px width=350px')
+	alert('img src="./наш кот.jpg" height=300px width=350px')
 })
